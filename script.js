a9e308c0132200bf67d8df280a0b40c3b6e3e9c7
Marina Rull Valdivieso <marinarullvaldivieso@Marinas-MacBook-Pro-26.local>
2023-10-13 12:04:19 +0200

    borrar commentarios

--- script.js
+++ script.js
@@ -2,26 +2,27 @@ document.addEventListener('DOMContentLoaded', function () {
     const fetchDataButton = document.querySelector('#enter');
     const urlInput = document.querySelector('.cajita');
     const responseData = document.querySelector('#responseData');
-    const errorContainer = document.querySelector('#errorContainer'); // Nuevo contenedor para el mensaje de error
+    const errorContainer = document.querySelector('#errorContainer');
     fetchDataButton.addEventListener('click', async () => {
         const apiUrl = urlInput.value;
         try {
             const response = await fetch(apiUrl);
             const data = await response.json();
             responseData.textContent = JSON.stringify(data, null, 2);
-            errorContainer.style.display = 'none'; // Ocultar el contenedor de error si hay éxito
+            errorContainer.style.display = 'none'; 
         } catch (error) {
-            responseData.textContent = ''; // Limpiar el contenido anterior
-            errorContainer.innerHTML = ''; // Limpiar cualquier contenido previo en el contenedor de error
+            responseData.textContent = '';
+            errorContainer.innerHTML = ''; 
             const errorMessage = document.createElement('p');
             errorMessage.textContent = '"Oops! I think I just hit the purr instead of the enter key. Meow-error!"';
             const errorGif = document.createElement('img');
-            errorGif.src = 'Imagenes/error404cat.gif'; // Reemplaza con la ruta de tu archivo GIF
+            errorGif.src = 'Imagenes/error404cat.gif';
             errorContainer.appendChild(errorMessage);
-            errorContainer.appendChild(errorGif); // Agregar el GIF al contenedor de error
-            errorContainer.style.display = 'block'; // Mostrar el contenedor de error
+            errorContainer.appendChild(errorGif); 
+            errorContainer.style.display = 'block'; 
         }
     });
 });
+
 
 
