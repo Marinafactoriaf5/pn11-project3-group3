caa8c76b3079b5295f6476b36532a95f3fde03c6
ccm24 <ccmm0024@gmail.com>
2023-10-13 11:49:12 +0200

    mensaje error

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-/*document.addEventListener('DOMContentLoaded', function () {
-    const fetchDataButton = document.querySelector('#enter');
-    const urlInput = document.querySelector('.cajita');
-    const responseData = document.querySelector('#responseData');
-    fetchDataButton.addEventListener('click', async () => {
-        const apiUrl = urlInput.value;
-        const errorImage = document.querySelector("#errorImage");
-        
-        try {
-            const response = await fetch(apiUrl);
-            const data = await response.json();
-            responseData.textContent = JSON.stringify(data, null, 2);
-            errorImage.style.display = "none"; // Oculta la imagen si se carga correctamente
-        } catch (error) {
-            responseData.textContent = 'Error al obtener los datos. Asegúrate de que la URL de la API sea válida.';
-            errorImage.style.display = "block";
-        }
-    });
-}); */
-
-
-
 document.addEventListener('DOMContentLoaded', function () {
     const fetchDataButton = document.querySelector('#enter');
     const urlInput = document.querySelector('.cajita');
@@ -36,12 +14,14 @@ document.addEventListener('DOMContentLoaded', function () {
             responseData.textContent = ''; // Limpiar el contenido anterior
             errorContainer.innerHTML = ''; // Limpiar cualquier contenido previo en el contenedor de error
             const errorMessage = document.createElement('p');
-            errorMessage.textContent = 'Error al obtener los datos. Asegúrate de que la URL de la API sea válida.';
+            errorMessage.textContent = '"Oops! I think I just hit the purr instead of the enter key. Meow-error!"';
             const errorGif = document.createElement('img');
-            errorGif.src = 'Imagenes/error404.gif'; // Reemplaza con la ruta de tu archivo GIF
+            errorGif.src = 'Imagenes/error404cat.gif'; // Reemplaza con la ruta de tu archivo GIF
             errorContainer.appendChild(errorMessage);
             errorContainer.appendChild(errorGif); // Agregar el GIF al contenedor de error
             errorContainer.style.display = 'block'; // Mostrar el contenedor de error
         }
     });
 });
+
+
